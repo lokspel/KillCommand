7f189b9777d90ec26bc5c0cbdd939be81280df26
lokspel <208148594+lokspel@users.noreply.github.com>
2026-03-19 10:52:48 +0400

    Restrict /kill tab completion by permission

--- src/main/java/me/miko/killcommand/KillCommand.java
+++ src/main/java/me/miko/killcommand/KillCommand.java
@@ -1,17 +1,16 @@
 package me.miko.killcommand;
 
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
 public class KillCommand extends JavaPlugin {
-    private final CommandExecutor killExecutor = new KillExecutor();
-
     @Override
     public void onEnable() {
         PluginCommand killCommand = Objects.requireNonNull(getCommand("kill"));
-        killCommand.setExecutor(killExecutor);
+        KillExecutor executor = new KillExecutor();
+        killCommand.setExecutor(executor);
+        killCommand.setTabCompleter(executor);
     }
 }
--- src/main/java/me/miko/killcommand/KillExecutor.java
+++ src/main/java/me/miko/killcommand/KillExecutor.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.damage.DamageSource;
 import org.bukkit.damage.DamageType;
 import org.bukkit.entity.Player;
@@ -13,7 +14,11 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.jspecify.annotations.NonNull;
 
-public class KillExecutor implements CommandExecutor {
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class KillExecutor implements CommandExecutor, TabCompleter {
     private static final DamageSource KILL_DAMAGE_SOURCE = DamageSource.builder(DamageType.GENERIC_KILL).build();
     private static final Component NO_PERMISSION_MESSAGE = Component.text(
             "I'm sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.",
@@ -37,5 +42,23 @@ public class KillExecutor implements CommandExecutor {
 
         player.damage(player.getHealth() + player.getAbsorptionAmount() + 1.0D, KILL_DAMAGE_SOURCE);
         return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (!sender.hasPermission("killcommand.kill.others")) {
+            return Collections.emptyList();
+        }
+
+        if (args.length != 1) {
+            return Collections.emptyList();
+        }
+
+        String input = args[0].toLowerCase();
+        return Bukkit.getOnlinePlayers().stream()
+                .map(Player::getName)
+                .filter(name -> name.toLowerCase().startsWith(input))
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .collect(Collectors.toList());
     }
 }
